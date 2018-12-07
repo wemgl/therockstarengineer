4120f1a5cd2e6bc6a46de8b36d063b9a5bf99835
Wembley Leach <wembley.gl@gmail.com>
2018-12-06 20:31:34 -0500

    Refactor password generation implementation to use enums

--- desktop-applications/src/main/java/com/therockstarengineer/desktop/controllers/PasswordController.java
+++ desktop-applications/src/main/java/com/therockstarengineer/desktop/controllers/PasswordController.java
@@ -1,5 +1,6 @@
 package com.therockstarengineer.desktop.controllers;
 
+import com.therockstarengineer.desktop.model.Password;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -8,33 +9,45 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.EnumSet;
 import java.util.ResourceBundle;
 
 public class PasswordController implements Initializable {
-
-    private enum GeneratorOption {
-        NUMBERS, SPECIAL_CHARACTERS;
-    }
+    private Password password;
 
     @FXML
-    public TextField password;
+    public TextField output;
 
     @FXML
-    public Button generateButton;
+    public Button generate;
 
     @FXML
-    public CheckBox includeNumbers;
+    public CheckBox digits;
 
     @FXML
-    public CheckBox includeSpecialChars;
+    public CheckBox specialChars;
+
+    @FXML
+    public CheckBox upperCase;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        password = new Password();
     }
 
     @FXML
     public void generatePassword(ActionEvent actionEvent) {
-        password.setText("Secure password");
+        var options = EnumSet.of(Password.Option.LOWERCASE_LETTERS);
+        if (upperCase.isSelected()) {
+            options.add(Password.Option.UPPERCASE_LETTERS);
+        }
+        if (digits.isSelected()) {
+            options.add(Password.Option.DIGITS);
+        }
+        if (specialChars.isSelected()) {
+            options.add(Password.Option.SPECIAL_CHARACTERS);
+        }
+        var output = password.generate(options);
+        this.output.setText(output);
     }
 }
---
+++ desktop-applications/src/main/java/com/therockstarengineer/desktop/model/Password.java
@@ -0,0 +1,72 @@
+package com.therockstarengineer.desktop.model;
+
+import java.util.Arrays;
+import java.util.Set;
+
+import static java.util.concurrent.ThreadLocalRandom.current;
+import static java.util.stream.IntStream.concat;
+import static java.util.stream.IntStream.range;
+
+/**
+ * Generates a strong output consisting of any combination of letters (upper and lowercase),
+ * numbers, and special characters. The output assumes only ASCII encoding.
+ *
+ * @see <a href="http://www.asciichart.com">Decimal ASCII Chart</a>
+ */
+public class Password {
+    private static final int DEFAULT_PASSWORD_LENGTH = 16;
+
+    /**
+     * Enumerates the set of options available for creating a strong password.
+     */
+    public enum Option {
+        /**
+         * The ASCII lowercase characters from a-z.
+         */
+        LOWERCASE_LETTERS(range(97, 122).toArray()),
+
+        /**
+         * The ASCII uppercase characters from A-Z.
+         */
+        UPPERCASE_LETTERS(range(65, 90).toArray()),
+
+        /**
+         * The ASCII digits from 0-9.
+         */
+        DIGITS(range(48, 57).toArray()),
+
+        /**
+         * Various special characters from different ranges of the ASCII encoding.
+         */
+        SPECIAL_CHARACTERS(concat(range(33, 47), range(58, 64)).toArray());
+
+        final int[] characters;
+
+        Option(int[] characters) {
+            this.characters = characters;
+        }
+    }
+
+    /**
+     * Generates a strong password consisting of characters from the available options.
+     *
+     * @param options All options the password can fulfill. A password may not have a
+     *                value from the given option.
+     * @return A strong password consisting of characters specified by the options.
+     */
+    public String generate(Set<Option> options) {
+        var characters = options.stream()
+                .flatMapToInt(option -> Arrays.stream(option.characters))
+                .toArray();
+        var password = new StringBuilder(DEFAULT_PASSWORD_LENGTH);
+        for (int i = 0; i < DEFAULT_PASSWORD_LENGTH; i++) {
+            password.append(nextCharacter(characters));
+        }
+        return password.toString();
+    }
+
+    private char nextCharacter(int[] characters) {
+        var digit = current().nextInt(characters.length);
+        return (char) characters[digit];
+    }
+}
